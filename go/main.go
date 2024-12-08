ca71a37d568226f8079fcd4dfaae30b1115b114d
machida <machida@kmc.gr.jp>
2024-12-08 12:12:17 +0900

    oldをけす

--- go/main.go
+++ go/main.go
@@ -102,7 +102,6 @@ func setup() http.Handler {
 		authedMux := mux.With(ownerAuthMiddleware)
 		authedMux.HandleFunc("GET /api/owner/sales", ownerGetSales)
 		authedMux.HandleFunc("GET /api/owner/chairs", ownerGetChairs)
-		authedMux.HandleFunc("GET /api/owner/chairs_old", ownerGetChairsOld)
 	}
 
 	// chair handlers
