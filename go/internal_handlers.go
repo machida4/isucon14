679f74796dcb42461de338cf9fcacdff56785826
laft2 <44256206+laft2@users.noreply.github.com>
2024-12-08 15:37:09 +0900

    matchingをマンハッタンベースに

--- go/internal_handlers.go
+++ go/internal_handlers.go
@@ -1,17 +1,24 @@
 package main
 
 import (
+	"context"
 	"database/sql"
 	"errors"
 	"net/http"
+	"sort"
 )
 
-// このAPIをインスタンス内から一定間隔で叩かせることで、椅子とライドをマッチングさせる
 func internalGetMatching(w http.ResponseWriter, r *http.Request) {
 	ctx := r.Context()
 	// MEMO: 一旦最も待たせているリクエストに適当な空いている椅子マッチさせる実装とする。おそらくもっといい方法があるはず…
-	ride := &Ride{}
-	if err := db.GetContext(ctx, ride, `SELECT * FROM rides WHERE chair_id IS NULL ORDER BY created_at LIMIT 1`); err != nil {
+	rides := []Ride{}
+
+	chairs := []Chair{}
+	if err := db.SelectContext(ctx, &chairs, "SELECT * FROM chairs c WHERE c.is_active = TRUE"); err != nil {
+
+	}
+
+	if err := db.SelectContext(ctx, &rides, `SELECT * FROM rides WHERE chair_id IS NULL AND latitude IS NOT NULL ORDER BY created_at`); err != nil {
 		if errors.Is(err, sql.ErrNoRows) {
 			w.WriteHeader(http.StatusNoContent)
 			return
@@ -20,34 +27,44 @@ func internalGetMatching(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	matched := &Chair{}
-	empty := false
-	for i := 0; i < 10; i++ {
-		if err := db.GetContext(ctx, matched, "SELECT * FROM chairs INNER JOIN (SELECT id FROM chairs WHERE is_active = TRUE ORDER BY RAND() LIMIT 1) AS tmp ON chairs.id = tmp.id LIMIT 1"); err != nil {
-			if errors.Is(err, sql.ErrNoRows) {
-				w.WriteHeader(http.StatusNoContent)
-				return
-			}
-			writeError(w, http.StatusInternalServerError, err)
-		}
-
-		if err := db.GetContext(ctx, &empty, "SELECT COUNT(*) = 0 FROM (SELECT COUNT(chair_sent_at) = 6 AS completed FROM ride_statuses WHERE ride_id IN (SELECT id FROM rides WHERE chair_id = ?) GROUP BY ride_id) is_completed WHERE completed = FALSE", matched.ID); err != nil {
-			writeError(w, http.StatusInternalServerError, err)
+	for _, v := range rides {
+		sort.Slice(chairs, func(i, j int) bool {
+			return calculateDistance(v.PickupLatitude, v.PickupLongitude, int(chairs[i].Latitude.Int64), int(chairs[i].Longitude.Int64)) < calculateDistance(v.PickupLatitude, v.PickupLongitude, int(chairs[j].Latitude.Int64), int(chairs[j].Longitude.Int64))
+		})
+		var i int
+		var matched bool
+		if i, matched = matchRide(ctx, v, chairs); !matched {
+			w.WriteHeader(http.StatusNoContent)
 			return
 		}
-		if empty {
-			break
+		chairs = append(chairs[:i], chairs[i+1:]...)
+	}
+	w.WriteHeader(http.StatusNoContent)
+}
+
+// rideを受け取って、マッチングさせる。マッチングできたらtrueを返す
+func matchRide(ctx context.Context, ride Ride, chairs []Chair) (int, bool) {
+	for i, chair := range chairs {
+		if isChairEmpty(ctx, chair) {
+			if err := saveMatchedRide(ctx, ride, chair); err != nil {
+				return i, false
+			}
+			return i, true
 		}
 	}
-	if !empty {
-		w.WriteHeader(http.StatusNoContent)
-		return
-	}
 
-	if _, err := db.ExecContext(ctx, "UPDATE rides SET chair_id = ? WHERE id = ?", matched.ID, ride.ID); err != nil {
-		writeError(w, http.StatusInternalServerError, err)
-		return
-	}
+	return len(chairs), false
+}
 
-	w.WriteHeader(http.StatusNoContent)
+func isChairEmpty(ctx context.Context, chair Chair) bool {
+	empty := false
+	if err := db.GetContext(ctx, &empty, "SELECT COUNT(*) = 0 FROM (SELECT COUNT(chair_sent_at) = 6 AS completed FROM ride_statuses WHERE ride_id IN (SELECT id FROM rides WHERE chair_id = ?) GROUP BY ride_id) is_completed WHERE completed = FALSE", chair.ID); err != nil {
+		return false
+	}
+	return empty
+}
+
+func saveMatchedRide(ctx context.Context, ride Ride, chair Chair) error {
+	_, err := db.ExecContext(ctx, "UPDATE rides SET chair_id = ? WHERE id = ?", chair.ID, ride.ID)
+	return err
 }
