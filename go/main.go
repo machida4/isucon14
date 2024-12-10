deaa4e19d3461fab36f9d7cc05bf3cce1a7ad252
isucon <isucon@example.com>
2024-12-10 15:22:33 +0000

    revert commit

--- go/main.go
+++ go/main.go
@@ -5,7 +5,6 @@ import (
 	crand "crypto/rand"
 	"encoding/json"
 	"fmt"
-	"log"
 	"log/slog"
 	"net"
 	"net/http"
@@ -19,10 +18,6 @@ import (
 	"github.com/go-chi/chi/v5/middleware"
 	"github.com/go-sql-driver/mysql"
 	"github.com/jmoiron/sqlx"
-
-	_ "net/http/pprof"
-
-	"github.com/felixge/fgprof"
 )
 
 var cache *freecache.Cache
@@ -30,11 +25,6 @@ var db *sqlx.DB
 var startedTime time.Time
 
 func main() {
-	http.DefaultServeMux.Handle("/debug/fgprof", fgprof.Handler())
-	go func() {
-		log.Println(http.ListenAndServe(":6060", nil))
-	}()
-
 	cache = freecache.NewCache(100 * 1024 * 1024) // 100MiB
 
 	mux := setup()
@@ -86,7 +76,7 @@ func setup() http.Handler {
 	db.SetMaxOpenConns(16)
 
 	mux := chi.NewRouter()
-	mux.Use(middleware.Logger)
+	// mux.Use(middleware.Logger)
 	mux.Use(middleware.Recoverer)
 	mux.HandleFunc("POST /api/initialize", postInitialize)
 
@@ -221,17 +211,17 @@ func writeJSON(w http.ResponseWriter, statusCode int, v interface{}) {
 }
 
 func writeError(w http.ResponseWriter, statusCode int, err error) {
-	w.Header().Set("Content-Type", "application/json;charset=utf-8")
-	w.WriteHeader(statusCode)
-	buf, marshalError := json.Marshal(map[string]string{"message": err.Error()})
-	if marshalError != nil {
-		w.WriteHeader(http.StatusInternalServerError)
-		w.Write([]byte(`{"error":"marshaling error failed"}`))
-		return
-	}
-	w.Write(buf)
+	// w.Header().Set("Content-Type", "application/json;charset=utf-8")
+	// w.WriteHeader(statusCode)
+	// buf, marshalError := json.Marshal(map[string]string{"message": err.Error()})
+	// if marshalError != nil {
+	// 	w.WriteHeader(http.StatusInternalServerError)
+	// 	w.Write([]byte(`{"error":"marshaling error failed"}`))
+	// 	return
+	// }
+	// w.Write(buf)
 
-	slog.Error("error response wrote", err)
+	// slog.Error("error response wrote", err)
 }
 
 func secureRandomStr(b int) string {
